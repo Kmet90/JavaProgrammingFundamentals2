0ce20d554f11a2372953fab5c444e01502c4f4b4
Kmet90 <kmet90@gmail.com>
2021-03-23 15:41:13 +0000

    JavaFundamentals

---
+++ src/RegularExpressionsLab/MatchPhoneNumber_02.java
@@ -0,0 +1,27 @@
+package RegularExpressionsLab;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchPhoneNumber_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String regex = "\\+359([ -])2\\1[0-9]{3}\\1[0-9]{4}\\b";
+        String input = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        List<String> matchedPhoneNumbers = new ArrayList<>();
+
+        while (matcher.find()) {
+            matchedPhoneNumbers.add(matcher.group());
+        }
+
+        System.out.println(String.join(", ", matchedPhoneNumbers));
+    }
+}
